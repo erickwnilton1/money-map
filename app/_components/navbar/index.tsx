829920a1d29cac1048a11505332be80f8a81179d
erick <erickwelberth1@hotmail.com>
2025-03-15 11:28:55 -0300

    feature to improve screen [login]

--- app/_components/navbar/index.tsx
+++ app/_components/navbar/index.tsx
@@ -3,8 +3,8 @@ import { WalletMinimal } from "lucide-react";
 const Navbar = () => {
   return (
     <nav className="flex px-8 py-2">
-      <div className="text-purple-800">
-        <WalletMinimal />
+      <div className="text-white">
+        <WalletMinimal size={30} />
       </div>
     </nav>
   );
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,15 @@ import Navbar from "../_components/navbar";
 import { Button } from "@/components/ui/button";
 import { SignInButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
-import { LogInIcon } from "lucide-react";
+import { SquarePlayIcon } from "lucide-react";
 import { redirect } from "next/navigation";
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 const LoginPage = async () => {
   const { userId } = await auth();
@@ -13,17 +20,41 @@ const LoginPage = async () => {
   }
 
   return (
-    <section className="h-screen flex flex-col">
+    <section className="h-screen flex flex-col bg-gradient-to-b from-black to-[#001f4a]">
       <Navbar />
-      <div className="flex w-full h-full items-center justify-center">
-        <div className="flex  items-center justify-center">
+      <div className="w-full h-full px-8 flex flex-col m-auto items-center justify-center gap-10">
+        <div>
+          <h1
+            className={`${inter.className} font-bold text-white text-[32px] lg:text-[42px] xl:text-[50px] leading-tight`}
+          >
+            O controle financeiro pessoal com toda a{" "}
+            <br className="hidden md:block" />
+            praticidade para{" "}
+            <span className="underline hover:text-[#081e9d]">
+              organizar
+            </span>{" "}
+            suas finanças.
+          </h1>
+        </div>
+        <div>
+          <p
+            className={`${inter.className} font-light text-[18px] lg:text-[25px] text-[#B1aaaa] sm:text-center`}
+          >
+            Na MAP você controla seu dinheiro em tempo real em uma solução
+            <br className="hidden md:block" />
+            completa, garantindo uma gestão financeira prática.
+          </p>
+        </div>
+        <div>
           <SignInButton>
             <Button
-              variant="outline"
-              className="w-[200px] text-purple-800 hover:text-purple-800 sm:w-[500px]"
+              className={`${inter.className} font-bold text-[15px] w-[250px] h-[50px] bg-[#081e9d] hover:bg-[#28089de5]`}
             >
-              <LogInIcon />
-              Fazer login ou criar conta
+              Teste gratuitamente
+              <SquarePlayIcon
+                style={{ width: "35px", height: "35px" }}
+                className="text-white"
+              />
             </Button>
           </SignInButton>
         </div>
